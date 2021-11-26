2b25243472d5b38d0648d347b39e2e998f48e6fc
Suyash Soni <suyash.soni248@gmail.com>
2021-11-26 15:24:14 +0530

    Update forti.sh

--- forti.sh
+++ forti.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/expect
 
-spawn /home/suyash/forticlientsslvpn/64bit/forticlientsslvpn_cli --server 14.98.58.218:10443 --vpnuser suyash.soni@whatfix.com
+spawn /home/suyash/forticlientsslvpn/64bit/forticlientsslvpn_cli --server <ip>:<port> --vpnuser suyash.soni@gmail.com
 expect "Password for VPN"
-send "WFXNW#@71suyash\r"
+send "pass1234\r"
 expect "Would you like to connect to this server? (Y/N)"
 send "Y\r"
 expect "STATUS::Tunnel running"
